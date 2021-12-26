fbb4b57daba663543d1bb0c2daab7467dabd90e3
sajad kardel <sajadkardel@outlook.com>
2021-12-26 04:39:56 +0330

    init files

---
+++ TestTemplate/TestTemplate.Ui/TestTemplateParameter.cs
@@ -0,0 +1,19 @@
+﻿namespace TestTemplate.Ui
+{
+    public class TestTemplateParameter
+    {
+        public void TestParameter()
+        {
+            Console.WriteLine("what is your best friend name? : {bestFriend}");
+            Console.WriteLine("what is your favorite food? : {foodName}");
+
+#if likeJob
+        Console.WriteLine("do you like your job? : Yes");
+#else
+            Console.WriteLine("do you like your job? : No");
+#endif
+
+
+        }
+    }
+}
